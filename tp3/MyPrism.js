f14dda7998dfaa471367588b314dbf640c3ebd06
Mariana Marujo Conde <up202108824@edu.fe.up.pt>
2025-03-18 23:24:54 +0000

    Prism Normals Implemented (Now Working W/ Different Stack Sizes)

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -22,42 +22,54 @@ export class MyPrism extends CGFobject {
         var ang = 0;
         var alphaAng = 2 * Math.PI / this.slices;
         var indexAux = 0;
+        for (var j = 0; j < this.stacks; j++){
+            // Defining Z Values
+            let zLower = j / this.stacks;
+            let zUpper = (j + 1) / this.stacks;
 
-        for (var i = 0; i < this.slices; i++) {
+            for (var i = 0; i < this.slices; i++) {
 
-            let sa = Math.sin(ang);
-            let saa = Math.sin(ang + alphaAng);
-            let ca = Math.cos(ang);
-            let caa = Math.cos(ang + alphaAng);
+                let sa = Math.sin(ang);
+                let saa = Math.sin(ang + alphaAng);
+                let ca = Math.cos(ang);
+                let caa = Math.cos(ang + alphaAng);
 
-            //Define Vertices for this Slice
-            this.vertices.push(ca, sa, 1);
-            this.vertices.push(ca, sa, 0);
-            this.vertices.push(caa, saa, 0);
-            this.vertices.push(caa, saa, 1);
+                //Define Vertices for this Slice
+                this.vertices.push(ca, sa, zUpper);
+                this.vertices.push(ca, sa, zLower);
+                this.vertices.push(caa, saa, zLower);
+                this.vertices.push(caa, saa, zUpper);
 
-            // Compute Normal Vectors
-            let normalX = (ca+caa)/2;
-            let normalY = (sa+saa)/2;
+                // Compute Normal Vectors
+                let normalX = (ca + caa) / 2;
+                let normalY = (sa + saa) / 2;
 
-            let normalLength = Math.sqrt(normalX**2 + normalY**2);
-            normalX /= normalLength;
-            normalY /= normalLength;
+                let normalLength = Math.sqrt(normalX ** 2 + normalY ** 2);
+                normalX /= normalLength;
+                normalY /= normalLength;
 
-            let normal = [normalX, normalY, 0];
+                let normal = [normalX, normalY, 0];
 
-            // Assign Normals
-            this.normals.push(...normal, ...normal, ...normal, ...normal);
+                // Assign Normals
+                this.normals.push(...normal, ...normal, ...normal, ...normal);
 
 
-            // Define Indices for this Face
-            this.indices.push(indexAux, indexAux + 1, indexAux + 2);
-            this.indices.push(indexAux, indexAux + 2, indexAux + 3);
-            indexAux += 4;
+                // Define Indices for this Face
+                this.indices.push(indexAux, indexAux + 1, indexAux + 2);
+                this.indices.push(indexAux, indexAux + 2, indexAux + 3);
 
-            ang += alphaAng;
+                if(j < this.stacks - 1){
+                    // Connect to next stack
+                    this.indices.push(indexAux+2, indexAux + 1, indexAux + 5);
+                    this.indices.push(indexAux+2, indexAux + 5, indexAux + 6);
+                    this.indices.push(indexAux+3, indexAux + 2, indexAux + 6);
+                    this.indices.push(indexAux+3, indexAux + 6, indexAux + 7);
+                }
+
+                indexAux += 4;
+                ang += alphaAng;
+            }
         }
-
 
         // this.normals.push(0, 0, 1);
 
