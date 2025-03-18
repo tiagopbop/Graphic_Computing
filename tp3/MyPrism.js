26f1edb86730449c52e5edace13799f0764769de
Mariana Marujo Conde <up202108824@edu.fe.up.pt>
2025-03-18 23:15:42 +0000

    Prism Normals Implemented

--- tp3/MyPrism.js
+++ tp3/MyPrism.js
@@ -1,4 +1,5 @@
 import {CGFobject} from '../lib/CGF.js';
+
 /**
  * MyDiamond
  * @constructor
@@ -14,88 +15,51 @@ export class MyPrism extends CGFobject {
     }
 
     initBuffers() {
-        // this.vertices = [
-        //     0, 0, 0,
-        //     0.5, Math.sin(60), 0,
-        //     -0.5, Math.sin(120), 0,
-        //     -1, 0, 0,
-        //     -0.5, Math.sin(240), 0,
-        //     0.5, Math.sin(300), 0,
-        //     1, 0, 0,
-        // ];
-
-        //Counter-clockwise reference of vertices
-        // this.indices = [
-        //     0, 1, 2,
-        //     0, 2, 3,
-        //     0, 3, 4,
-        //     0, 4, 5,
-        //     0, 5, 6,
-        // ];
-
-        // this.normals = [
-        // ]
-
         this.vertices = [];
         this.indices = [];
         this.normals = [];
 
         var ang = 0;
-        var alphaAng = 2*Math.PI/this.slices;
+        var alphaAng = 2 * Math.PI / this.slices;
         var indexAux = 0;
-        for(var i = 0; i < this.slices; i++){
 
-            /*var sa=Math.sin(ang);
-            var saa=Math.sin(ang+alphaAng);
-            var ca=Math.cos(ang);
-            var caa=Math.cos(ang+alphaAng);*/
+        for (var i = 0; i < this.slices; i++) {
+
+            let sa = Math.sin(ang);
+            let saa = Math.sin(ang + alphaAng);
+            let ca = Math.cos(ang);
+            let caa = Math.cos(ang + alphaAng);
+
+            //Define Vertices for this Slice
+            this.vertices.push(ca, sa, 1);
+            this.vertices.push(ca, sa, 0);
+            this.vertices.push(caa, saa, 0);
+            this.vertices.push(caa, saa, 1);
+
+            // Compute Normal Vectors
+            let normalX = (ca+caa)/2;
+            let normalY = (sa+saa)/2;
+
+            let normalLength = Math.sqrt(normalX**2 + normalY**2);
+            normalX /= normalLength;
+            normalY /= normalLength;
+
+            let normal = [normalX, normalY, 0];
+
+            // Assign Normals
+            this.normals.push(...normal, ...normal, ...normal, ...normal);
 
 
-            this.vertices.push(Math.cos(ang), Math.sin(ang), 1);
-            this.vertices.push(Math.cos(ang), Math.sin(ang), 0);
-            ang+=alphaAng;
+            // Define Indices for this Face
+            this.indices.push(indexAux, indexAux + 1, indexAux + 2);
+            this.indices.push(indexAux, indexAux + 2, indexAux + 3);
+            indexAux += 4;
 
-            this.vertices.push(Math.cos(ang), Math.sin(ang), 0);
-            this.vertices.push(Math.cos(ang), Math.sin(ang), 1);
-
-
-
-
-            /*// triangle normal computed by cross product of two edges
-            var normal= [
-                saa-sa,
-                ca*saa-sa*caa,
-                caa-ca
-            ];
-
-
-            // normalization
-            var nsize=Math.sqrt(
-                normal[0]*normal[0]+
-                normal[1]*normal[1]+
-                normal[2]*normal[2]+
-                normal[3]*normal[3]
-            );
-            normal[0]/=nsize;
-            normal[1]/=nsize;
-            normal[2]/=nsize;
-            normal[3]/=nsize;
-
-            // push normal once for each vertex of this triangle
-            this.normals.push(...normal);
-            this.normals.push(...normal);
-            this.normals.push(...normal);
-            this.normals.push(...normal);*/
-
-            this.indices.push(indexAux,indexAux+1,indexAux+2);
-            this.indices.push(indexAux,indexAux+2,indexAux+3);
-            indexAux+=4;
+            ang += alphaAng;
         }
 
 
-        this.normals.push(0,0,1);
-
-
+        // this.normals.push(0, 0, 1);
 
 
         //The defined indices (and corresponding vertices)
